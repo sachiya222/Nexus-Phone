7e8b296836d6ebb316e46691cc3b9c2456ff98cb
sachiya222 <espressionsdesigner@gmail.com>
2026-06-08 15:02:27 +0400

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,34 +1,29 @@
+-- client/main.lua
 local phoneOpen = false
 
--- Command to open the phone
 RegisterCommand('nexus_openphone', function()
     if not phoneOpen then
-        phoneOpen = true
-        
-        -- Give the player's mouse/keyboard focus to the UI
-        SetNuiFocus(true, true)
-        
-        -- Send a message to our app.js to show the screen
-        SendNUIMessage({
-            type = "openPhone"
-        })
+        -- Fetch player data from our new server script
+        lib.callback('nexus_phone:server:GetPlayerProfile', false, function(playerData)
+            if playerData then
+                phoneOpen = true
+                SetNuiFocus(true, true)
+                
+                -- Send the data to our visual screen
+                SendNUIMessage({
+                    type = "openPhone",
+                    player = playerData
+                })
+            end
+        end)
     end
 end, false)
 
--- Bind the command to the 'M' key by default
 RegisterKeyMapping('nexus_openphone', 'Open Nexus Phone', 'keyboard', 'M')
 
--- Callback from the UI when the Escape key is pressed
 RegisterNUICallback('closePhone', function(data, cb)
     phoneOpen = false
-    
-    -- Give controls back to the player's character
     SetNuiFocus(false, false)
-    
-    -- Tell the UI to hide itself
-    SendNUIMessage({
-        type = "closePhone"
-    })
-    
+    SendNUIMessage({ type = "closePhone" })
     cb('ok')
 end)
